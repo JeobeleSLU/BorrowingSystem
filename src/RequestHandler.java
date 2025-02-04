feb461aa7932ca59a648bfc7e079b2531986ea3a
Jeobele <jeobelecedeno121304@gmail.com>
2025-02-04 22:22:11 +0800

    add: todo

---
+++ src/RequestHandler.java
@@ -0,0 +1,8 @@
+/**
+ * This guy would be our request controller guy, will handle how the client will do everything
+ * this little guy would be passed in to our clients on servers hehe like dis
+ * ClientHandler(Request Handler )
+ */
+public class RequestHandler {
+
+}
