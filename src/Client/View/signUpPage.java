02e9d6c155f8e63f525108fdd6a82ebfb0802d95
Jhezreen <2242815@slu.edu.ph>
2025-02-16 17:59:05 +0800

    fields

--- src/Client/View/signUpPage.java
+++ src/Client/View/signUpPage.java
@@ -15,8 +15,8 @@ public class signUpPage extends JFrame {
     private JTextField emailField;
     private JLabel fNameLbl;
     private JLabel lNameLbl;
-    private JTextField firstNameLbl;
-    private JTextField lastNameLbl;
+    private JTextField firstNameField;
+    private JTextField lastNameField;
 
     public signUpPage() {
         setContentPane(loginPanel);
@@ -54,6 +54,18 @@ public class signUpPage extends JFrame {
 
         //EMAIL FIELD
         emailField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        firstNameField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        lastNameField.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
