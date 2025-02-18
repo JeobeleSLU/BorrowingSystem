b243fd86587eb010e6632b7ff5cc13d42ef888e6
CONTILLO <2241591@slu.edu.ph>
2025-02-19 00:11:28 +0800

    start stop server

--- src/Server/Network/Server.java
+++ src/Server/Network/Server.java
@@ -1,13 +1,11 @@
+
 package Server.Network;
 
 import Client.Model.EquipmentManager;
-import Client.Service.SingletonSocketService;
 import Server.Controller.TransactionController;
 import Server.Model.Authenticator;
-import Server.Network.ClientHandler;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -20,7 +18,6 @@ public class Server {
         TransactionController controller = new TransactionController();
         EquipmentManager equipmentManager = new EquipmentManager();
         Authenticator authenticator = new Authenticator();
-        new Thread(new ServerHandler.serverHandler()).start();
         ServerSocket server = new ServerSocket(6969);
         ExecutorService threadPool = Executors.newFixedThreadPool(20);
         while (true) {
